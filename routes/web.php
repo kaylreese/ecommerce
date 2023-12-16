cba3965ad9f835ca29d32a6df3b37c65143232f9
Samuel Bocanegra Delgado <samuel.bocanegrad@gmail.com>
2023-12-16 17:53:36 -0500

    login and dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('admin', function () {
+    return view('admin.auth.login');
+});
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
