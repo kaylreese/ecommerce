1359d0bf790effed182a5f2567b005950d327a07
Samuel Bocanegra Delgado <samuel.bocanegrad@gmail.com>
2024-01-05 19:30:49 -0500

    header and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,6 @@
 
         @include('layouts._mobile')
     
-        <!-- Sign in / Register Modal -->
         <div class="modal fade" id="signin-modal" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
@@ -61,12 +60,12 @@
                                             <div class="form-group">
                                                 <label for="singin-email">Username or email address *</label>
                                                 <input type="text" class="form-control" id="singin-email" name="singin-email" required>
-                                            </div><!-- End .form-group -->
+                                            </div>
     
                                             <div class="form-group">
                                                 <label for="singin-password">Password *</label>
                                                 <input type="password" class="form-control" id="singin-password" name="singin-password" required>
-                                            </div><!-- End .form-group -->
+                                            </div>
     
                                             <div class="form-footer">
                                                 <button type="submit" class="btn btn-outline-primary-2">
@@ -77,12 +76,12 @@
                                                 <div class="custom-control custom-checkbox">
                                                     <input type="checkbox" class="custom-control-input" id="signin-remember">
                                                     <label class="custom-control-label" for="signin-remember">Remember Me</label>
-                                                </div><!-- End .custom-checkbox -->
+                                                </div>
     
                                                 <a href="#" class="forgot-link">Forgot Your Password?</a>
-                                            </div><!-- End .form-footer -->
+                                            </div>
                                         </form>
-                                        <div class="form-choice">
+                                        {{-- <div class="form-choice">
                                             <p class="text-center">or sign in with</p>
                                             <div class="row">
                                                 <div class="col-sm-6">
@@ -90,27 +89,28 @@
                                                         <i class="icon-google"></i>
                                                         Login With Google
                                                     </a>
-                                                </div><!-- End .col-6 -->
+                                                </div>
                                                 <div class="col-sm-6">
                                                     <a href="#" class="btn btn-login btn-f">
                                                         <i class="icon-facebook-f"></i>
                                                         Login With Facebook
                                                     </a>
-                                                </div><!-- End .col-6 -->
-                                            </div><!-- End .row -->
-                                        </div><!-- End .form-choice -->
-                                    </div><!-- .End .tab-pane -->
+                                                </div>
+                                            </div>
+                                        </div> --}}
+                                    </div>
+
                                     <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="register-tab">
                                         <form action="#">
                                             <div class="form-group">
                                                 <label for="register-email">Your email address *</label>
                                                 <input type="email" class="form-control" id="register-email" name="register-email" required>
-                                            </div><!-- End .form-group -->
+                                            </div>
     
                                             <div class="form-group">
                                                 <label for="register-password">Password *</label>
                                                 <input type="password" class="form-control" id="register-password" name="register-password" required>
-                                            </div><!-- End .form-group -->
+                                            </div>
     
                                             <div class="form-footer">
                                                 <button type="submit" class="btn btn-outline-primary-2">
@@ -121,10 +121,10 @@
                                                 <div class="custom-control custom-checkbox">
                                                     <input type="checkbox" class="custom-control-input" id="register-policy" required>
                                                     <label class="custom-control-label" for="register-policy">I agree to the <a href="#">privacy policy</a> *</label>
-                                                </div><!-- End .custom-checkbox -->
-                                            </div><!-- End .form-footer -->
+                                                </div>
+                                            </div>
                                         </form>
-                                        <div class="form-choice">
+                                        {{-- <div class="form-choice">
                                             <p class="text-center">or sign in with</p>
                                             <div class="row">
                                                 <div class="col-sm-6">
@@ -132,23 +132,23 @@
                                                         <i class="icon-google"></i>
                                                         Login With Google
                                                     </a>
-                                                </div><!-- End .col-6 -->
+                                                </div>
                                                 <div class="col-sm-6">
                                                     <a href="#" class="btn btn-login  btn-f">
                                                         <i class="icon-facebook-f"></i>
                                                         Login With Facebook
                                                     </a>
-                                                </div><!-- End .col-6 -->
-                                            </div><!-- End .row -->
-                                        </div><!-- End .form-choice -->
-                                    </div><!-- .End .tab-pane -->
-                                </div><!-- End .tab-content -->
-                            </div><!-- End .form-tab -->
-                        </div><!-- End .form-box -->
-                    </div><!-- End .modal-body -->
-                </div><!-- End .modal-content -->
-            </div><!-- End .modal-dialog -->
-        </div><!-- End .modal -->
+                                                </div>
+                                            </div>
+                                        </div> --}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     
         <div class="container newsletter-popup-container mfp-hide" id="newsletter-popup-form">
             <div class="row justify-content-center">
@@ -164,17 +164,17 @@
                                         <input type="email" class="form-control form-control-white" placeholder="Your Email Address" aria-label="Email Adress" required>
                                         <div class="input-group-append">
                                             <button class="btn" type="submit"><span>go</span></button>
-                                        </div><!-- .End .input-group-append -->
-                                    </div><!-- .End .input-group -->
+                                        </div>
+                                    </div>
                                 </form>
                                 <div class="custom-control custom-checkbox">
                                     <input type="checkbox" class="custom-control-input" id="register-policy-2" required>
                                     <label class="custom-control-label" for="register-policy-2">Do not show this popup again</label>
-                                </div><!-- End .custom-checkbox -->
+                                </div>
                             </div>
                         </div>
                         <div class="col-xl-2-5col col-lg-5 ">
-                            <img src="page/images/popup/newsletter/img-1.jpg" class="newsletter-img" alt="newsletter">
+                            <img src="{{ url('public/page/images/popup/newsletter/img-1.jpg') }}" class="newsletter-img" alt="newsletter">
                         </div>
                     </div>
                 </div>
